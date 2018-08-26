27cd3b94b24f1de3081afe9d67c74c374dac4983
LRKirkendoll <l.r.kirkendoll@gmail.com>
2018-08-25 19:30:47 -0500

    hw

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+var alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesSoFar = [];
+var userGuess = null;
+var letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+
+
+document.onkeyup = function(event) {
+
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	if (guessesSoFar.indexOf(userGuess) < 0 && alphabetLetters.indexOf(userGuess) >= 0) {
+		guessesSoFar[guessesSoFar.length]=userGuess;
+		guessesLeft--;
+	}
+
+	if (letterToBeGuessed == userGuess) {
+		wins++;
+		guessesLeft = 9;
+		guessesSoFar = [];
+		letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+		
+	}
+
+	if (guessesLeft == 0) {
+		losses++;
+		guessesLeft = 9;
+		guessesSoFar = [];
+		letterToBeGuessed = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
+		
+	}
+
+	var html = "<p><h1>The Psychic Game</h1></p>" + "<p><h4>Guess what letter I\'m thinking of</h4></p>" + "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + "<p><h4>Your guesses so far: " + guessesSoFar + "</h4></p>";
+	document.querySelector("#game").innerHTML = html;
+
+}
